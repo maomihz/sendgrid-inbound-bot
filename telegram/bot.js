f83080db7c0c4e158f80b4f01ab4d0a9eb0919b8
MaomiHz <maomihz@gmail.com>
2020-03-14 00:15:12 -0500

    Add start and help command

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,8 +1,10 @@
 const Telegraf = require('telegraf');
 const config = require('../config/config');
+const Context = require('./context');
 
-const bot = new Telegraf(config.token);
+const bot = new Telegraf(config.token, { contextType: Context });
 
-bot.start((ctx) => ctx.reply('hello, world'));
+bot.start(ctx => ctx.replyWithTemplate('start.ejs'));
+bot.help(ctx => ctx.replyWithTemplate('help.ejs'));
 
 module.exports = bot;
---
+++ telegram/context.js
@@ -0,0 +1,22 @@
+const Telegraf = require('telegraf');
+
+class MyContext extends Telegraf.Context {
+    constructor (update, telegram, options) {
+        super(update, telegram, options)
+    }
+
+
+    replyWithTemplate(template, extra) {
+        this.telegram.response.render(template, (err, str) => {
+            if (!err) {
+                this.reply(str, {
+                    parse_mode: 'HTML',
+                    disable_web_page_preview: true,
+                    ...extra
+                });
+            }
+        });
+    }
+}
+
+module.exports = MyContext;
